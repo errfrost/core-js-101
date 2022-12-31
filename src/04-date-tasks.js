608bebfb212d6fd8e5767ab65df40c5ccc1469e5
Vladimir Kurganov <err0rfrost@gmail.com>
2022-12-31 16:19:34 +0600

    feat: date4

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -77,8 +77,13 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const diff = endDate - startDate;
+  const milliSecondes = (diff % 1000).toString().padStart(3, '0');
+  const seconds = Math.floor((diff / 1000) % 60).toString().padStart(2, '0');
+  const minutes = Math.floor((diff / 1000 / 60) % 60).toString().padStart(2, '0');
+  const hours = Math.floor(diff / 1000 / 60 / 60).toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}.${milliSecondes}`;
 }
 
 
