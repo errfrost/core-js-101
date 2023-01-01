c3a87654a288ba5ef713ed8edac3115ae859ea01
Vladimir Kurganov <err0rfrost@gmail.com>
2023-01-01 23:14:05 +0600

    feat: 07-12

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -290,8 +290,10 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let sum = String(num).split('').map((i) => Number(i)).reduce((acc, cur) => acc + cur);
+  if (sum > 9) sum = getDigitalRoot(sum);
+  return sum;
 }
 
 
