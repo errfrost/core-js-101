880a91690b0d3403d55e7fe19b437ae94506bc6a
Vladimir Kurganov <err0rfrost@gmail.com>
2023-01-02 02:17:57 +0600

    feat: 08-8

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -188,8 +188,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let res = startFrom;
+  return () => {
+    res += 1;
+    return res - 1;
+  };
 }
 
 
