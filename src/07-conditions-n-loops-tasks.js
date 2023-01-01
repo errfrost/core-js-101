56f278e57b21f3a3ea9947d69bae17a889cd0332
Vladimir Kurganov <err0rfrost@gmail.com>
2023-01-01 16:54:25 +0600

    feat: 07-7

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -172,8 +172,11 @@ function isInsideCircle(circle, point) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str.lastIndexOf(str[i]) === str.indexOf(str[i])) return str[i];
+  }
+  return null;
 }
 
 
