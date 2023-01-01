a6b913408d0b7e13b143f9ab99fc801e2682bd1d
Vladimir Kurganov <err0rfrost@gmail.com>
2023-01-02 01:37:45 +0600

    feat: 08-4

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -88,8 +88,9 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cacheFunc = func();
+  return () => cacheFunc;
 }
 
 
